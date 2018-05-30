5c08f1e48f3ebb6c936440e0c4abac1b0b46653f
Arora <sarora@dayforce.com>
2018-05-30 13:31:35 -0400

    Adding new files 30-May-2018

--- MyCoypuTest/Page/LoginPage/Login.cs
+++ MyCoypuTest/Page/LoginPage/Login.cs
@@ -1,17 +1,23 @@
-﻿using static MyCoypuTest.Core.DriverFactory.BrowserSessionConfig;
+﻿using System;
+using Coypu;
+using static MyCoypuTest.Core.DriverFactory.BrowserSessionConfig;
 using static MyCoypuTest.Util.TestPage;
 
 namespace MyCoypuTest.Page.LoginPage
 {
     public class Login
     {
-        public static void BrowserLogin(string url, string username, string password, string loginButton)
+        public static void BrowserLogin(string url, string username, string password, string loginButton, string browser = "chrome")
         {
-            SetBrowserSession("executeautomation.com", "firefox");
+            SetBrowserSession("executeautomation.com", browser);
             PageBrowser.Visit(url);
             PageBrowser.FillIn("UserName").With(username);
             PageBrowser.FillIn("Password").With(password);
-            PageBrowser.ClickButton(loginButton);
+            PageBrowser.ClickButton(loginButton, new Options()
+            {
+                WaitBeforeClick = TimeSpan.FromMilliseconds(2000),
+
+            });
 
         }
     }
--- MyCoypuTest/UnitTest1.cs
+++ MyCoypuTest/UnitTest1.cs
@@ -1,8 +1,11 @@
-﻿using NUnit.Framework;
+﻿using Coypu;
+using Coypu.Matchers;
+using NUnit.Framework;
 using static MyCoypuTest.Core.DriverFactory.BrowserSessionConfig;
 using static MyCoypuTest.Page.LoginPage.Login;
 using static MyCoypuTest.Page.UserFormPage.UserForm;
 using static MyCoypuTest.Util.TestPage;
+using Shows = Coypu.NUnit.Matchers.Shows;
 
 
 namespace MyCoypuTest
@@ -37,9 +40,26 @@ namespace MyCoypuTest
             PageBrowser.FindId("Automation Tools").Hover();
             PageBrowser.FindId("Selenium").Hover();
             PageBrowser.FindId("Selenium RC").Hover().Click();
-
+            Assert.That(PageBrowser.HasContent("Selenium RC", Options.Substring));
 
         }
+
+        [Test]
+        public void Test_MenubarMenu()
+        {
+            BrowserLogin("/demosite/Login.html", "admin", "password", "Login");
+            PageBrowser.FindId("Automation Tools").Hover();
+            Assert.That(PageBrowser, Shows.Css("ul > li > a > span", $"Selenium"));
+            Assert.That(PageBrowser, Shows.Css("ul > li > a > span", $"BDD"));
+        }
+
+        [Test]
+        public void Test_WaitMechanism()
+        {
+            BrowserLogin("/demosite/Login.html", "admin", "password", "Login");
+
+        }
+
 
     }
 }
